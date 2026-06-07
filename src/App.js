0358ad439648bc45ee956514d238b26b56f8e71b
Subhash <subhashkumar65143@gmail.com>
2026-06-07 12:25:52 +0530

    Added progress Bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo,deleteTodo,completeTodo,unmarkTodo } from "./slices/todoSlice";
+import {
+  addTodo,
+  deleteTodo,
+  completeTodo,
+  unmarkTodo,
+} from "./slices/todoSlice";
 import { v4 as uuid } from "uuid";
 import Navbar from "./components/navbar";
 import "./App.css";
@@ -15,7 +20,7 @@ function App() {
         addTodo({
           id: uuid(),
           todo: inputText,
-          status:"added"
+          status: "added",
         }),
       );
     }
@@ -23,32 +28,46 @@ function App() {
   };
 
   const onDeleteTodoClick = (id) => {
-    dispatch(deleteTodo({
-      id:id,
-    }))
-  }
+    dispatch(
+      deleteTodo({
+        id: id,
+      }),
+    );
+  };
   const onCompleteTodoClick = (id) => {
-    dispatch(completeTodo({
-      id:id,
-    }))
-  }
+    dispatch(
+      completeTodo({
+        id: id,
+      }),
+    );
+  };
   const onUnmarkTodoClick = (id) => {
-    dispatch(unmarkTodo({
-      id:id,
-    }))
+    dispatch(
+      unmarkTodo({
+        id: id,
+      }),
+    );
+  };
+  const countCompleted = () => {
+    let count = 0;
+    todos.forEach((todo) => {
+      if(todo.status==="complete") count++;
+    })
+    console.log(count);
+    return count;
   }
   return (
-  <>
-    <div className="min-h-screen pt-24 bg-slate-900 text-white flex flex-col overflow-hidden">
-      <Navbar />
+    <>
+      <div className="min-h-screen pt-24 bg-slate-900 text-white flex flex-col overflow-hidden">
+        <Navbar />
 
-      {/* Input Section */}
-      <div className="flex flex-col sm:flex-row justify-center gap-3 px-4 w-full max-w-3xl mx-auto">
-        <input
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          placeholder="Enter a todo..."
-          className="
+        {/* Input Section */}
+        <div className="flex flex-col sm:flex-row justify-center gap-3 px-4 w-full max-w-3xl mx-auto">
+          <input
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Enter a todo..."
+            className="
             bg-slate-800 border border-slate-700
             rounded-xl px-4 py-3
             flex-1
@@ -56,47 +75,50 @@ function App() {
             focus:border-blue-500
             transition
           "
-        />
+          />
 
-        <button
-          onClick={onAddTodoClick}
-          className="
+          <button
+            onClick={onAddTodoClick}
+            className="
             px-5 py-3 rounded-xl
             bg-blue-600 hover:bg-blue-500
             transition font-semibold
             w-full sm:w-auto
           "
-        >
-          Add Todo
-        </button>
-      </div>
-
-      {/* Todo List */}
-
-      <div
-        className="
+          >
+            Add Todo
+          </button>
+        </div>
+        {/* Todo List */}
+        <div
+          className="
         overflow-y-auto
         mt-4
         px-3 sm:px-6
         pb-6
         h-[calc(100vh-220px)]
-
+        
         w-full
         sm:w-[90%]
         md:w-[75%]
         lg:w-1/2
-
+        
         mx-auto
         "
-      >
-        <div className="space-y-4">
-
-          {(todos && todos.length > 0) ? (
-            todos.map(({ id, todo, status }) => (
-
-              <div
-                key={id}
-                className="
+        >
+          <div className="space-y-4">
+            {(todos && todos.length > 0 )? (
+              <div className="flex justify-end font-black mt-4 mb-4">
+                Completed : {countCompleted()}/{todos.length}
+              </div>
+            ) : (
+              <div className="flex justify-end font-black mt-4 mb-4"></div>
+            )}
+            {todos && todos.length > 0 ? (
+              todos.map(({ id, todo, status }) => (
+                <div
+                  key={id}
+                  className="
                 bg-slate-800
                 border border-slate-700
                 rounded-2xl
@@ -105,27 +127,24 @@ function App() {
                 hover:border-slate-500
                 transition-all
                 "
-              >
-
-                <div className="flex flex-col sm:flex-row justify-between gap-4">
-
-                  <h2
-                    className={`
+                >
+                  <div className="flex flex-col sm:flex-row justify-between gap-4">
+                    <h2
+                      className={`
                     text-lg sm:text-xl
                     font-semibold
                     break-all
                     ${status === "complete" ? "line-through" : ""}
                   `}
-                  >
-                    {todo}
-                  </h2>
+                    >
+                      {todo}
+                    </h2>
 
-                  <div className="flex flex-wrap gap-2 sm:justify-end">
-
-                    {status !== "complete" ? (
-                      <button
-                        onClick={() => onCompleteTodoClick(id)}
-                        className="
+                    <div className="flex flex-wrap gap-2 sm:justify-end">
+                      {status !== "complete" ? (
+                        <button
+                          onClick={() => onCompleteTodoClick(id)}
+                          className="
                         px-4 py-2
                         rounded-xl
                         bg-green-300/20
@@ -133,13 +152,13 @@ function App() {
                         transition
                         min-w-[110px]
                         "
-                      >
-                        Mark
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => onUnmarkTodoClick(id)}
-                        className="
+                        >
+                          Mark
+                        </button>
+                      ) : (
+                        <button
+                          onClick={() => onUnmarkTodoClick(id)}
+                          className="
                         px-4 py-2
                         rounded-xl
                         bg-green-300/20
@@ -147,14 +166,14 @@ function App() {
                         transition
                         min-w-[110px]
                         "
-                      >
-                        Unmark
-                      </button>
-                    )}
+                        >
+                          Unmark
+                        </button>
+                      )}
 
-                    <button
-                      onClick={() => onDeleteTodoClick(id)}
-                      className="
+                      <button
+                        onClick={() => onDeleteTodoClick(id)}
+                        className="
                       px-4 py-2
                       rounded-xl
                       bg-red-500/20
@@ -162,29 +181,21 @@ function App() {
                       hover:bg-red-500/30
                       transition
                       "
-                    >
-                      Delete
-                    </button>
-
+                      >
+                        Delete
+                      </button>
+                    </div>
                   </div>
-
                 </div>
-
-              </div>
-
-            ))
-          ) : (
-            <h1 className="text-center font-black text-2xl">
-              No Todos !!
-            </h1>
-          )}
-
+              ))
+            ) : (
+              <h1 className="text-center font-black text-2xl">No Todos !!</h1>
+            )}
+          </div>
         </div>
       </div>
-
-    </div>
-  </>
-);
+    </>
+  );
 }
 
 export default App;
